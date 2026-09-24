cd3f7e4fb35d7b4cf1a54cc7c64fae94025d4ca9
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2026-09-24 09:36:40 -0300

    Initial commit

---
+++ Assets/Scripts/AgentBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentBehavior : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform target;
+    public Transform target2;
+    public Transform targetAux;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetAux = target;
+        agent.destination = targetAux.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (agent.hasPath && agent.remainingDistance < 1)
+        {
+            if(targetAux == target)
+            {
+                targetAux = target2;
+            }
+            else
+            {
+                targetAux = target;
+            }
+            agent.destination = targetAux.position;
+        }
+    }
+}
---
+++ Assets/Scripts/ArrayExamples.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayExamples : MonoBehaviour
+{
+    public int[] edades = new int[4];
+    public int[] notas;
+    public int sumaDeLasEdades;
+    public int cantReprobados;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sumaDeLasEdades = SumaValoresDelArray(edades);
+        cantReprobados = CantidadDeReprobados(notas);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    int SumaValoresDelArray(int[] arr)
+    {
+        int resultado = 0;
+
+        for (int i = 0;i<arr.Length;i++ )
+        {
+            resultado = resultado + arr[i];
+        }
+
+        return resultado;
+    }
+
+    int CantidadDeReprobados(int[] valores)
+    {
+        int reprobados = 0;
+        for (int i = 0; i< valores.Length; i++)
+        {
+            if (valores[i] < 6)
+            {
+                reprobados++;
+            }
+        }
+        return reprobados;
+    }
+}
