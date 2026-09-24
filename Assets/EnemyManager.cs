80dc5dd7e36d0c548144ab73dd492b044db7cc22
Davidna9 <davoo78909@gmail.com>
2026-09-24 10:42:19 -0300

    hpla

---
+++ Assets/EnemyManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public Enemy[] enemies;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemies = FindObjectsOfType<Enemy>();
+        SetDamagePointsTo(0);
+    }
+
+
+    void SetDamagePointsTo(int newValue)
+    {
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            enemies[i].damagePoints = newValue;
+        }
+    }
+}
